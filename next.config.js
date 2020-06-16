22b28ca8c799a814fb688c30c2ed45b8039e1593
Jair Medeiros <jmferreira70@gmail.com>
2020-06-16 07:52:44 -0300

    Adicionando compatibilidade com SCSS/SASS no Next.js

---
+++ next.config.js
@@ -0,0 +1,5 @@
+const withPlugins = require('next-compose-plugins');
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+
+module.exports = withPlugins([[withCSS], [withSass]]);
