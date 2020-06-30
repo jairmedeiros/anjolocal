91aafebbe1a9d3909b08a0f801b1ad7513aacdf5
Jair Medeiros <jmferreira70@gmail.com>
2020-06-30 08:45:46 -0300

    [WIP] Separando reducers e actions em diretório separados e inicando a criação da manipulação dos dados de localização.

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,44 +1,12 @@
 import { useMemo } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
 
 let store;
-const initialState = {
-  lastUpdate: 0,
-  light: false,
-  count: 0,
-};
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'TICK':
-      return {
-        ...state,
-        lastUpdate: action.lastUpdate,
-        light: !!action.light,
-      };
-    case 'INCREMENT':
-      return {
-        ...state,
-        count: state.count + 1,
-      };
-    case 'DECREMENT':
-      return {
-        ...state,
-        count: state.count - 1,
-      };
-    case 'RESET':
-      return {
-        ...state,
-        count: initialState.count,
-      };
-    default:
-      return state;
-  }
-};
-
-function initStore(preloadedState = initialState) {
-  return createStore(reducer, preloadedState, composeWithDevTools(applyMiddleware()));
+function initStore(preloadedState = {}) {
+  return createStore(reducers, preloadedState, composeWithDevTools(applyMiddleware()));
 }
 
 export const initializeStore = (preloadedState) => {
