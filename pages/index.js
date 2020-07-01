6a3d3cd8655769e0a4614fcabf8d998acc79e9d0
Jair Medeiros <jmferreira70@gmail.com>
2020-07-01 08:53:34 -0300

    Criando comportamento fixed para o header e adicionando botão, background, shadow e etc.

--- pages/index.js
+++ pages/index.js
@@ -29,55 +29,9 @@ const IndexPage = () => {
   return (
     <div className="body-wrap">
       <header className="site-header">
-        <div className="header-shape header-shape-1">
-          <svg width={337} height={222} viewBox="0 0 337 222" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <linearGradient x1="50%" y1="55.434%" x2="50%" y2="0%" id="header-shape-1">
-                <stop stopColor="#E0E1FE" stopOpacity={0} offset="0%" />
-                <stop stopColor="#E0E1FE" offset="100%" />
-              </linearGradient>
-            </defs>
-            <path
-              d="M1103.21 0H1440v400h-400c145.927-118.557 166.997-251.89 63.21-400z"
-              transform="translate(-1103)"
-              fill="url(#header-shape-1)"
-              fillRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="header-shape header-shape-2">
-          <svg
-            width={128}
-            height={128}
-            viewBox="0 0 128 128"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ overflow: 'visible' }}
-          >
-            <defs>
-              <linearGradient
-                x1="93.05%"
-                y1="19.767%"
-                x2="15.034%"
-                y2="85.765%"
-                id="header-shape-2"
-              >
-                <stop stopColor="#FF3058" offset="0%" />
-                <stop stopColor="#FF6381" offset="100%" />
-              </linearGradient>
-            </defs>
-            <circle
-              className="anime-element fadeup-animation"
-              cx={64}
-              cy={64}
-              r={64}
-              fill="url(#header-shape-2)"
-              fillRule="evenodd"
-            />
-          </svg>
-        </div>
         <div className="container">
           <div className="site-header-inner">
-            <div className="brand header-brand">
+            <div className="header-left-elements">
               <h1 className="m-0">
                 <a href="#">
                   <svg width={32} height={32} xmlns="http://www.w3.org/2000/svg">
@@ -138,9 +92,62 @@ const IndexPage = () => {
                 </a>
               </h1>
             </div>
+            <div className="header-right-elements">
+              <a className="button button-primary button-block button-shadow" href="#">
+                Early access
+              </a>
+            </div>
           </div>
         </div>
       </header>
+      <div className="header-background-elements">
+        <div className="header-shape header-shape-1">
+          <svg width={337} height={222} viewBox="0 0 337 222" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <linearGradient x1="50%" y1="55.434%" x2="50%" y2="0%" id="header-shape-1">
+                <stop stopColor="#E0E1FE" stopOpacity={0} offset="0%" />
+                <stop stopColor="#E0E1FE" offset="100%" />
+              </linearGradient>
+            </defs>
+            <path
+              d="M1103.21 0H1440v400h-400c145.927-118.557 166.997-251.89 63.21-400z"
+              transform="translate(-1103)"
+              fill="url(#header-shape-1)"
+              fillRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="header-shape header-shape-2">
+          <svg
+            width={128}
+            height={128}
+            viewBox="0 0 128 128"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ overflow: 'visible' }}
+          >
+            <defs>
+              <linearGradient
+                x1="93.05%"
+                y1="19.767%"
+                x2="15.034%"
+                y2="85.765%"
+                id="header-shape-2"
+              >
+                <stop stopColor="#FF3058" offset="0%" />
+                <stop stopColor="#FF6381" offset="100%" />
+              </linearGradient>
+            </defs>
+            <circle
+              className="anime-element fadeup-animation"
+              cx={64}
+              cy={64}
+              r={64}
+              fill="url(#header-shape-2)"
+              fillRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
       <main>
         <section className="hero">
           <div className="container">
