a3d61f9c059515dbb94efe5f0f1c0d547f6c1b02
Jair Medeiros <jmferreira70@gmail.com>
2020-07-07 12:30:06 -0300

    Alterado texto do botão do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -70,7 +70,7 @@ const Header = () => {
             </div>
             <div className="header-right-elements">
               <Button majorStyle="primary" url="#" useShadow>
-                Preciso de doação
+                Cadastrar instituição
               </Button>
             </div>
           </div>
