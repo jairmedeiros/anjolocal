5f7a6679d9974c7cc97c330e4d6ba2e7fdb71679
Jair Medeiros <jmferreira70@gmail.com>
2020-07-19 11:33:07 -0300

    Removedo comentarios

--- pages/index.js
+++ pages/index.js
@@ -10,29 +10,4 @@ function Index() {
   );
 }
 
-// export async function getStaticProps() {
-//   const reduxStore = initializeStore();
-//   const apolloClient = initializeApollo();
-//   const { dispatch } = reduxStore;
-
-//   dispatch({
-//     type: 'TICK',
-//     light: true,
-//     lastUpdate: Date.now(),
-//   });
-
-// await apolloClient.query({
-//   query: ALL_POSTS_QUERY,
-//   variables: allPostsQueryVars,
-// });
-
-//   return {
-//     props: {
-//       initialReduxState: reduxStore.getState(),
-//       initialApolloState: apolloClient.cache.extract(),
-//     },
-//     unstable_revalidate: 1,
-//   };
-// }
-
 export default Index;
